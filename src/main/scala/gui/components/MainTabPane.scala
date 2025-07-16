40d616ff04b20ec31ebaac6f708c6c7f95bcdc50
KevinTan2025 <KevinTan2025@users.noreply.github.com>
2025-07-16 09:45:54 +0800

    Add MainTabPane component as the main tab container for the application

---
+++ src/main/scala/gui/components/MainTabPane.scala
@@ -0,0 +1,23 @@
+package gui.components
+
+import scalafx.scene.control._
+import scalafx.scene.layout.Region
+
+/**
+ * Main tab container for the application
+ */
+class MainTabPane extends BaseComponent {
+  
+  override def build(): Region = {
+    new TabPane {
+      tabs = Seq(
+        new DashboardComponent().build(),
+        new AnnouncementsTab().build(),
+        new FoodSharingTab().build(),
+        new DiscussionTab().build(),
+        new EventsTab().build(),
+        new NotificationsTab().build()
+      )
+    }
+  }
+}
